a43796f8f006c70c32d82193353aec984737bc17
svjatber <svjat.ber3003@gmail.com>
2020-12-16 18:01:04 +0200

    'homework1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface User{
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit{
+
+  Users: User[];
+  user: User | null = null;
+
+    ngOnInit() {
+      this.Users = [{id: 1, name: 'Svjat'}, {id: 2, name: 'Loha'}, { id: 3, name: 'Solya'}];
+    }
+
+  showUser(user: User) {
+    console.log('post', user);
+    this.user = user
+  }
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {User} from '../app.component';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() user: User
+  @Output() onAdd: EventEmitter<User> = new EventEmitter<User>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showUser() {
+    this.onAdd.emit(this.user)
+  }
+}
